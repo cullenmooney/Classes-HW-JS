8c20a4d03985f6b179b10351f0b06761bc20df33
Cullen Mooney <cullenmooney@Cullens-MacBook.local>
2017-09-20 16:37:04 -0500

    created pet class

---
+++ main.js
@@ -0,0 +1,18 @@
+// Cullen Mooney Classes HW
+
+// #1.)
+
+class Pet {
+	constructor(name) {
+		this.name = name;
+		this.owner = '';
+	}
+	setOwner(nameIt) {
+		this.owner = nameIt;
+	}
+}
+const bobby = new Pet('bobby')
+bobby;
+
+// #2.)
+
