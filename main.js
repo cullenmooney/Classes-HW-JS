9b929d8ec628879ffb4bfbdabc694896ccbbc9c3
Cullen Mooney <cullenmooney@Cullens-MacBook.local>
2017-09-20 18:41:03 -0500

    created person class

--- main.js
+++ main.js
@@ -56,5 +56,73 @@ class Cat extends Pet {
 const sprinkles = new Cat('sprinkles', 300);
 sprinkles.getPrice();
 
+// # 6.)
+class Person {
+	constructor(name) {
+		this.name = name;
+		this.age = 0;
+		this.weight = 0;
+		this.mood = 0;
+		this.pets = [];
+		this.bankAccount = 0;
+	}
+	getName() {
+		this.name;
+	}
+	getAge() {
+		this.age;
+	}
+	getWeight() {
+		this.weight;
+	}
+	greet (otherPerson) {
+		console.log('hi ' + otherPerson)
+	}
+	eat () {
+		this.weight += 1;
+		this.mood += 1;
+	}
+	exercise () {
+		this.weight -=1;
+	}
+	ageing () {
+		this.age +=1;
+		this.weight +=1;
+		this.mood -=1;
+		this.bankAccount +=10;
+
+	}
+	buyPet(pet) {
+		this.pets.push(pet);
+		this.mood += 10;
+		this.bankAccount - pet.getPrice();
+	}
+}
+
+// #7.)
+const jill = new Person('jill');
+jill.getName();
+jill.getAge();
+jill.getWeight();
+jill.greet('cullen');
+jill.eat();
+jill.exercise();
+jill.ageing();
+jill.buyPet(sprinkles);
+jill;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
