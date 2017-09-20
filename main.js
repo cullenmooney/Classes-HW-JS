d1b4e6d3732f3f4c8d50576a76cb0c12aad2b916
Cullen Mooney <cullenmooney@Cullens-MacBook.local>
2017-09-20 16:49:31 -0500

    created dog class

--- main.js
+++ main.js
@@ -15,4 +15,25 @@ const bobby = new Pet('bobby')
 bobby;
 
 // #2.)
+class Dog extends Pet {
+	constructor(name, price) {
+		super(name)
+		this.price = price;
+	}
+	bark() {
+		console.log('bark');
+	}
+	chaseTail() {
+		console.log('oh boy oh boy oh boy')
+	}
+	getPrice (price) {
+		return this.price;
+	}
+}
+
+// #3.)
+const sparky = new Dog('sparky', 500)
+sparky.getPrice();
+
+
 
